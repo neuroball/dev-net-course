b4e9b0406c61bdbe1d182d79ecbb432b55529361
Schnarchendorf, Oliver <neuroball@gmail.com>
2021-10-08 20:59:18 -0700

    {second commit] Implement create_fortune_cookie_message function.

--- intro-python/part2/fortune_cookie.py
+++ intro-python/part2/fortune_cookie.py
@@ -31,11 +31,7 @@ def create_fortune_cookie_message(how_many_lucky_numbers: int) -> str:
 
     The message should include the user's fortune and lucky numbers.
     """
-    # TODO: Create a fortune cookie message by calling generate_fortune() and
-    # generate_lucky_numbers() and then composing and returning the fortune
-    # cookie's message.
-
-    raise NotImplementedError()
+    return 'Fortune: {} -  Lucky Numbers: {}'.format(generate_fortune(), generate_lucky_numbers(how_many_lucky_numbers))
 
 
 def main():
